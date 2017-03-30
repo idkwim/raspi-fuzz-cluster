02226fefe7a1e7a25330aad9c0824015d1d400bb
hugsy <hugsy@blah.cat>
2017-03-29 21:24:07 -0700

    improved make.sh template

--- .bashrc
+++ .bashrc
@@ -10,7 +10,8 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH=}:/usr/local/lib
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 export PATH=$PATH:$HOME/bin
 
-alias l='/bin/ls --color=always --group-directories-first--color=auto'
+alias ls='/bin/ls --color=always --group-directories-first'
+alias l='ls'
 alias ll='l -l --human-readable'
 alias a='ll --all'
 alias gdb="gdb -q"
--- libfuzzer/libexpat/make.sh
+++ libfuzzer/libexpat/make.sh
@@ -1,35 +1,65 @@
 #!/bin/bash
+#
+# Simple template for compiling target with libfuzzer
+#
 
 set -e
+# set -x
+
+info()    { echo -e "\e[0;34m[*]\e[0m $*"; }
+success() { echo -e "\e[0;32m[+]\e[0m $*"; }
+warn()    { echo -e "\e[0;33m[!]\e[0m $*"; }
+err()     { echo -e "\e[0;31m[!]\e[0m $*"; }
+
+build_libfuzzer()
+{
+    info "Downloading libfuzzer"
+    svn checkout --quiet http://llvm.org/svn/llvm-project/llvm/trunk/lib/Fuzzer
+    info "Compiling libfuzzer"
+    mkdir -p build && cd build && rm -fr .svn
+    ${CC} -g -c -O2 -std=c++11 ../Fuzzer/*.cpp -IFuzzer
+    info "Building libfuzzer library"
+    ar ruv libFuzzer.a Fuzzer*.o >/dev/null 2>&1
+    cd ..
+    success "libfuzzer built!"
+}
+
+if [ $# -lt 1 ]; then
+    err "Missing argument"
+    exit 1
+fi
 
 CC=`which clang++-3.9`
 IN="`realpath \"$1\"`"
 OUT=${IN/.cc/}
 
+if [ "$1" = "clean" ]; then
+    info "Cleaning stuff"
+    rm -fr -- build Fuzzer *.o
+    success "Done"
+    exit 0
+fi
+
 if [ ! -x "${CC}" ]; then
-    echo "[-] Invalid clang"
-    echo '[-] Run `apt-get install clang-3.8`'
+    err "Missing ${CC}"
+    warn 'Run `apt-get install clang-3.9`'
     exit 1
 fi
 
 shift
 LIBS="$@"
-LIBFUZZ="build/libFuzzer.a"
+LIBFUZZ="Fuzzer/libFuzzer.a"
 CPU="`grep --count processor /proc/cpuinfo`"
+FLAGS="-ggdb -O1 -fno-omit-frame-pointer -fsanitize-coverage=trace-cmp -fsanitize=undefined -fno-sanitize-recover=undefined -fsanitize-coverage=edge  -fsanitize=address"
 
-if [ ! -f ${LIBFUZZ} ]; then
-    echo "[+] Downloading libfuzzer"
-    svn co http://llvm.org/svn/llvm-project/llvm/trunk/lib/Fuzzer
-    echo "[+] Building libfuzzer library"
-    mkdir -p build && cd build
-    ${CC} -c -g -O2 -std=c++11 ../Fuzzer/*.cpp -IFuzzer
-    ar ruv libFuzzer.a Fuzzer*.o
-    cd ..
+[ ! -f ${LIBFUZZ} ] && build_libfuzzer
+
+info "Building '${OUT}'"
+${CC} ${FLAGS} ${IN} ${LIBFUZZ} ${LIBS} -o ${OUT}
+
+success "Success, now you can:"
+echo -e "- Start a simple one-core fuzzing run by running \n\t $ ${OUT}"
+if [ ${CPU} -gt 1 ]; then
+    echo -e "- Or use your ${CPU} cores in parallel by running \n\t $ ${OUT} -workers=${CPU} -jobs=${CPU} -timeout=3000"
 fi
-
-echo "[+] Building fuzzer"
-${CC} -ggdb -O1 -fno-omit-frame-pointer -fsanitize-coverage=trace-cmp -fsanitize=undefined -fno-sanitize-recover=undefined -fsanitize-coverage=edge  -fsanitize=address ${IN} ${LIBFUZZ} ${LIBS} -o ${OUT}
-
-echo "[+] Start a simple fuzzing run with '${OUT}'"
-echo "[+] Or using your ${CPU} cores in parallel: '${OUT} -workers=${CPU} -jobs=${CPU} -timeout=3000'"
 exit 0

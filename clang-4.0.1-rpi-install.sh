7d4ac329fe09d79b6826a360d3829eb929387cde
crazy rabbidz <hugsy@users.noreply.github.com>
2017-06-29 16:16:44 -0700

    Create clang-4.0.1-rpi-install.sh

---
+++ clang-4.0.1-rpi-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+url="http://releases.llvm.org/4.0.1/clang+llvm-4.0.1-armv7a-linux-gnueabihf.tar.xz"
+prefix="$(basename ${url/.tar.xz/})"
+
+pushd .
+cd /tmp
+wget ${url}
+tar -C /usr -xvJf ${prefix}.tar.xz --transform="flags=r;s!${prefix}!!"
+rm ${prefix}.tar.xz
+popd
+
+exit 0
